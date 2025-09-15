ec7bbe09d6ab6964d5b423965e5e81f9d835ee4e
alsl524 <osmm0617@gmail.com>
2025-09-15 14:54:22 +0900

    게임제작_3주차

---
+++ Assets/03wnck/script/Rktl.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TriangleMove : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3 (transform.position.x - 0.05f, transform.position.y, transform.position.z);
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("tkrwprl"))
+            {
+            Destroy(gameObject);
+        }   
+    }
+}
---
+++ Assets/03wnck/script/todtjdrl.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class todtjdrl : MonoBehaviour
+{
+    public GameObject todtjdrl_;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (a)
+        {
+            GameObject Ekrl = lnstantiate(todtjdrl);
+
+            a = false;
+        }
+    }
+}
