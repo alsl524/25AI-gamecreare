9b716a2eb7f939c146f0be8339d52c8e1549b14c
alsl524 <osmm0617@gmail.com>
2025-09-23 15:01:59 +0900

    게임제작_4주차_2번째

---
+++ Assets/4wnck/script/tkwn_Rktl.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class tkwn_TriangleMove : MonoBehaviour
+{
+    public float speed = 5f; // 이동 속도
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    private void Update()
+    {
+        // speed를 이용해 왼쪽으로 이동
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Player 태그 가진 오브젝트와 충돌하면
+        {
+            Time.timeScale = 0f; // 게임 시간 멈춤
+            Debug.Log("Player hit spike! Game Paused.");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("tkrwprl"))
+        {
+            Destroy(gameObject); // 삼각형 제거
+            Debug.Log("Spike : 소멸");
+        }
+    }
+}
---
+++ Assets/4wnck/script/tkwn_dbwjzjsxmfhf.cs
@@ -0,0 +1,51 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class tkwn_dbwjzjsxmfhf : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool isJumping = true;
+    public float JumpPower = 5.0f;
+
+    public GameObject text;
+
+    Animator animator; // ✅ Animator로 변경
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        isJumping = true ;
+        Debug.Log("Player : isJumping = true");
+        text.SetActive(false);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("EKd"))
+        {
+            Debug.Log("Player : 땅이당");
+            isJumping = false;
+            Debug.Log("Player : isJumping = false");
+        }
+        if (collision.gameObject.CompareTag("wnrdla"))
+        {
+            Debug.Log("Player : 게임 오버 충돌!");
+            text.SetActive(true);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !isJumping)
+        {
+            Debug.Log("Player : 점프!");
+            animator.Play("wjavm", -1, 0f); // ✅ Animator State 실행
+            rb.linearVelocity = new Vector2(0.0f, JumpPower);
+            isJumping = true;
+            Debug.Log("Player : isJumping = true");
+        }
+    }
+
+}
